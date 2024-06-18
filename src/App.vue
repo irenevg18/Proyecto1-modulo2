5ad40fb5ecfa4466c4b001286c35d4f68b12c5dc
irenevg18 <irenevalleguillen@gmail.com>
2024-06-18 10:33:10 +0200

    setting up

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-emerald-800 font-bold underline">{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Hello class",
+    };
+  },
+};
+</script>
